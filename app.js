7200369a3ac02b402b6c99ee97dde5b744913166
Russell Driver <russell_driver@ymail.com>
2022-10-18 07:25:21 +0100

    assigned front end WebSocket implementation

--- app.js
+++ app.js
@@ -40,30 +40,31 @@ httpServer = http.createServer(async (req, res) => {
         })
         res.end(JSON.stringify({"error":"error Russell"}))
     }
-
     
-    // let ws = new WebSocketServer({
-    //     "httpServer":httpServer
-    // });
+    let ws = new WebSocketServer({
+        "httpServer":httpServer
+    });
 
-    // ws.on('request', (r) => {
-    //     console.log('new request received');
-    //     connection = r.accept(null, r.origin);
+    ws.on('request', (r) => {
+        console.log('new request received');
+        connection = r.accept(null, r.origin);
         
-    //     connection.on('open', () => {
-    //         console.log(`connection opened`) 
-    //     })
-    //     connection.on('close', () => {
-    //         console.log('connection closed');
-    //     })
-    //     connection.on('message', (m) => {
-    //         console.log(`message received from client: ${m.utf8Data}`);
-    //         let connectionsArray = ws.connections;
-    //         connectionsArray.forEach((connection) => {
-    //             connection.send(`message from user: ${m.utf8Data}`)
-    //         })
-    //     })
-    // })
+        connection.on('open', () => {
+            console.log(`connection opened`) 
+        })
+        connection.on('close', () => {
+            console.log('connection closed');
+        })
+        connection.on('message', (m) => {
+            console.log(`message received from client: ${m.utf8Data}`);
+            let connectionsArray = ws.connections;
+            connectionsArray.forEach((connection) => {
+                connection.send(`message from user: ${m.utf8Data}`)
+            })
+        })
+    })
+
+
 });
 
 let PORT = process.env.PORT || 3000;
