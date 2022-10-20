9d11f1e894912b710bc208b6ca2f2f6161b98c03
Russell Driver <russell_driver@ymail.com>
2022-10-20 06:40:19 +0100

    added styles to username choice box, fixed bug in which ws connection was in incorrect scope

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ if(process.env.NODE_ENV !== 'production') require('dotenv').config()
 const http = require('http');
 const WebSocketServer = require('websocket').server;
 let jwt = require('jsonwebtoken');
-let connection;
 
-
-
-httpServer = http.createServer(async (req, res) => {
+const httpServer = http.createServer(async (req, res) => {
     if(req.method === 'OPTIONS') {
         const optionsHeaders = {
             "Access-Control-Allow-Origin":"*",
@@ -40,36 +37,36 @@ httpServer = http.createServer(async (req, res) => {
         })
         res.end(JSON.stringify({"error":"error Russell"}))
     }
-    
-    let ws = new WebSocketServer({
-        "httpServer":httpServer
-    });
+});
 
-    ws.on('request', (r) => {
-        console.log('new request received');
-        connection = r.accept(null, r.origin);
-        
-        connection.on('open', () => {
-            console.log(`connection opened`) 
-        })
-        connection.on('close', () => {
-            console.log('connection closed');
-        })
-        connection.on('message', (m) => {
-            console.log(`message received from client: ${m.utf8Data}`);
-            let connectionsArray = ws.connections;
-            connectionsArray.forEach((connection) => {
-                connection.send(`message from user: ${m.utf8Data}`)
-            })
+let ws = new WebSocketServer({
+    "httpServer":httpServer
+});
+ws.on('request', (r) => {
+    console.log('new request received');
+    let connection = r.accept(null, r.origin);
+    
+    connection.on('open', () => {
+        console.log(`connection opened`) 
+    })
+    connection.on('close', () => {
+        console.log('connection closed');
+    })
+    connection.on('message', (m) => {
+        console.log(`message received from client: ${m.utf8Data}`);
+        let connectionsArray = ws.connections;
+        connectionsArray.forEach((connection) => {
+            connection.send(`${m.utf8Data}`)
         })
     })
-
-
-});
+})
 
 let PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
+
+
+
 
